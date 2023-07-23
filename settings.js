b23dfb2d840e44b033d000d6afbf56776626ed4d
saadoux1 <139818001+saadoux1@users.noreply.github.com>
2023-07-23 19:16:02 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -28,7 +28,7 @@ global.CAPTION = '*© 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 𝙾𝙿𝙴
 > Ai Bot Password Get From Watching This Video 
 */
 global.AI_PASSWORD = ''
-global.SESSION_ID = ''
+global.SESSION_ID = 'elisa_md@;;;wQ1nAAzI#VFLYZ4jErswyxUqQ8jHeQ3UW-6CwAtpZtwptCnmnhEw'
 
 /*
 🎲 𝗢𝗧𝗛𝗘𝗥 𝗦𝗘𝗧𝗜𝗡𝗚𝗦
